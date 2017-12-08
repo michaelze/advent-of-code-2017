f5050318560ffb14f528943284e8a7d7f7b3b2c2
Michael Zender <michael@crazymonkeys.de>
2017-12-08 09:46:23 +0100

    Adds solution for second puzzle on day 4.

--- day-4/passwords.py
+++ day-4/passwords.py
@@ -13,6 +13,32 @@ class Password:
                 seenWords.append(word)
         return True
 
+    def checkAnagram(password):
+        words = password.split(' ')
+        letterCounts = []
+        for word in words:
+            letterDict = {}
+            for letter in word:
+                if letter in letterDict:
+                    letterDict[letter] = letterDict[letter] + 1
+                else:
+                    letterDict[letter] = 1
+            letterCounts.append(letterDict)
+        letterCountsLength = len(letterCounts)
+        for index in range(0, letterCountsLength - 1):
+            for compIndex in range(index + 1, letterCountsLength):
+                letterCount = letterCounts[index]
+                compLetterCount = letterCounts[compIndex]
+                if len(letterCount) == len(compLetterCount):
+                    anagram = True
+                    for letter, count in letterCount.items():
+                        if not letter in compLetterCount or letterCount[letter] != compLetterCount[letter]:
+                            anagram = False
+                            break
+                    if anagram:
+                        return False
+        return True
+
 
 class TestPassword(unittest.TestCase):
 
@@ -21,13 +47,26 @@ class TestPassword(unittest.TestCase):
         self.assertFalse(Password.check('aa bb cc dd aa'))
         self.assertTrue(Password.check('aa bb cc dd aaa'))
 
+    def testCheckAnagram(self):
+        self.assertTrue(Password.checkAnagram('abcde fghij'))
+        self.assertFalse(Password.checkAnagram('abcde xyz ecdab'))
+        self.assertTrue(Password.checkAnagram('a ab abc abd abf abj'))
+        self.assertTrue(Password.checkAnagram('iiii oiii ooii oooi oooo'))
+        self.assertFalse(Password.checkAnagram('oiii ioii iioi iiio'))
 
-if __name__ == '__main__':
+
+def checkPasswordFile(checkFunc):
     correctPasswords = 0
     with open('passwords.txt') as passwordsFile:
         passwords = passwordsFile.readlines()
         for password in passwords:
-            if Password.check(password.strip()):
+            if checkFunc(password.strip()):
                 correctPasswords = correctPasswords + 1
-    print ('correct passwords: ' + str(correctPasswords))
+    return correctPasswords
+
+
+if __name__ == '__main__':
+    correctPasswords1 = checkPasswordFile(lambda x: Password.check(x))
+    correctPasswords2 = checkPasswordFile(lambda x: Password.checkAnagram(x))
+    print ('correct passwords1: ' + str(correctPasswords1) + ' correct passwords2: ' + str(correctPasswords2))
     unittest.main()
